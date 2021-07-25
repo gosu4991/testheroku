cd8564ab47f5614ce0115f84a0aeb32aa06bb7cf
gosu4991 <87606753+gosu4991@users.noreply.github.com>
2021-07-24 23:00:55 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 from flask_restful import Resource, Api, reqparse
 from werkzeug.datastructures import FileStorage
-from prediction import predict
 import tempfile
 import json
 import pprint
@@ -27,24 +26,7 @@ class Hello(Resource):
         return {'word': '[tiger_cat,0.5858129858970642],[Egyptian_cat,0.210690438747406],'}
 
 
-
-
-class Image(Resource):
-
-    def post(self):
-        args = parser.parse_args()
-        the_file = args['file']
-        # save a temporary copy of the file
-        ofile, ofname = tempfile.mkstemp()
-        the_file.save(ofname)
-
-        return predict(ofname)
-
-
-
-
 api.add_resource(Hello, '/hello')
-api.add_resource(Image, '/image')
 
 if __name__ == '__main__':
     app.run(debug=True)
