99844cb066ec9718dc84b2f82df7786af7237cb1
gosu4991 <87606753+gosu4991@users.noreply.github.com>
2021-07-24 22:37:27 -0700

    Update app.py

--- app.py
+++ app.py
@@ -28,8 +28,41 @@ class Hello(Resource):
 
 
 
-api.add_resource(Hello, '/hello')
 
+class Image(Resource):
+
+    def post(self):
+        args = parser.parse_args()
+        the_file = args['file']
+        # save a temporary copy of the file
+        ofile, ofname = tempfile.mkstemp()
+        the_file.save(ofname)
+
+        return predict(ofname)
+
+
+
+# routes
+@app.route("/index.html", methods=['GET', 'POST'])
+def main():
+    return render_template("index.html")
+
+@app.route("/submit", methods = ['POST'])
+def get_output():
+    if request.method == 'POST':
+        img = request.files['my_image']
+        img_path = "static/" + img.filename
+        files = {'file': img.read()}
+        prediction = requests.post(url,files=files)
+        img.save(img_path) 
+        data = prediction.content
+        json_data = json.loads(data)
+
+
+    return render_template("index.html",data=json_data, img_path = img_path)
+
+api.add_resource(Hello, '/hello')
+api.add_resource(Image, '/image')
 
 if __name__ == '__main__':
     app.run(debug=True)
